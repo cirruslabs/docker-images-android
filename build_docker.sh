98411e50d64709f0ddc21118f48553215b1b3a3e
fedor <fedor.korotkov@gmail.com>
2018-03-05 10:11:38 -0500

    move Dockerfiles from https://github.com/cirruslabs/docker-images

---
+++ build_docker.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker build --tag cirrusci/android-sdk:tools sdk/tools
+docker build --tag cirrusci/android-sdk:26 sdk/26
+docker build --tag cirrusci/android-sdk:27 sdk/27
---
+++ push_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$CIRRUS_BRANCH" != "master" ]
+then
+    exit 0
+fi
+
+docker login --username $DOCKER_USER_NAME --password $DOCKER_PASSWORD
+
+docker push cirrusci/android-sdk:tools
+docker push cirrusci/android-sdk:26
+docker push cirrusci/android-sdk:27
