2c0d191ad36be0a0a592670d141d6e981aeae842
Fedor Korotkov <fedor.korotkov@gmail.com>
2018-12-11 21:46:09 -0500

    android-sdk:28

--- push_docker.sh
+++ push_docker.sh
@@ -10,5 +10,4 @@ fi
 docker login --username $DOCKER_USER_NAME --password $DOCKER_PASSWORD
 
 docker push cirrusci/android-sdk:tools
-docker push cirrusci/android-sdk:26
-docker push cirrusci/android-sdk:27
+docker push cirrusci/android-sdk:28
