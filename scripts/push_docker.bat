0936869eccf8361ad81f212eb72c0156191945ca
Fedor Korotkov <fedor.korotkov@gmail.com>
2019-02-15 16:19:50 -0500

    don't forget to push sdk container

--- scripts/push_docker.bat
+++ scripts/push_docker.bat
@@ -3,3 +3,4 @@ if NOT "%CIRRUS_BRANCH%" == "master" exit 0
 docker login --username %DOCKER_USER_NAME% --password %DOCKER_PASSWORD%
 
 docker push cirrusci/android-sdk:tools-windowsservercore-ltsc2016
+docker push cirrusci/android-sdk:28-windowsservercore-ltsc2016
