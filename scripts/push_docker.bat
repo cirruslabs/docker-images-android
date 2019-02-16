b3c8f131b8a01df7cf71345818923d1202930cda
Fedor Korotkov <fedor.korotkov@gmail.com>
2019-02-15 23:35:18 -0500

    fixed push script

--- scripts/push_docker.bat
+++ scripts/push_docker.bat
@@ -1,6 +1,17 @@
-if NOT "%CIRRUS_BRANCH%" == "master" exit 0
+@echo off
 
-docker login --username %DOCKER_USER_NAME% --password %DOCKER_PASSWORD%
+IF not "%CIRRUS_BRANCH%" == "master" GOTO NOTMASTER
+:YESPATH
+
+docker login --username "%DOCKER_USER_NAME%" --password "%DOCKER_PASSWORD%"
 
 docker push cirrusci/android-sdk:tools-windowsservercore-ltsc2016
 docker push cirrusci/android-sdk:28-windowsservercore-ltsc2016
+
+GOTO END
+:NOTMASTER
+
+@ECHO Not a master build. Nothing to push!
+
+GOTO END
+:END
