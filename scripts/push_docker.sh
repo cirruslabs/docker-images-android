00002a4147049858fa9215a63d5b29ece39f4be8
fkorotkov <fedor.korotkov@gmail.com>
2020-06-12 09:03:43 -0400

    Android 30

--- scripts/push_docker.sh
+++ scripts/push_docker.sh
@@ -10,4 +10,4 @@ fi
 docker login --username $DOCKER_USER_NAME --password $DOCKER_PASSWORD
 
 docker push cirrusci/android-sdk:tools
-docker push cirrusci/android-sdk:29
+docker push cirrusci/android-sdk:30
